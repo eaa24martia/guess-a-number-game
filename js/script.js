c434346351c5aada1223279737b3d3c4365a688a
eaa24martia <eaa24martia@gmail.com>
2025-01-27 21:03:35 +0100

    final

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 "use strict";
 
+let gamerunning = true;
+let min = 0;
+let max = 100;
+let RandomNum = Math.floor(Math.random() * (max - min)) + min;
+
+while (gamerunning) {
+  let guess = parseInt(prompt(`Guess a number between ${min} and ${max}`));
+
+  if (guess === RandomNum) {
+    alert(`Congratulations! The correct number is ${RandomNum}`);
+    gamerunning = false;
+  } else if (guess < RandomNum) {
+    alert(`The number is too low!`);
+  } else if (guess > RandomNum) {
+    alert(`The number is too high!`);
+  }
+}
